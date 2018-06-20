27170499257c5cd51e88ee80a2da721393616134
FrederickIge <duelarm@gmail.com>
2018-06-20 18:42:16 -0400

    mailchimp ajax on desktop

--- js/main.js
+++ js/main.js
@@ -79,17 +79,55 @@ app.service('anchorSmoothScroll', function(){
   $rootScope.src = 'https://upload.wikimedia.org/wikipedia/commons/3/30/Nuvola_filesystems_services.svg';
 }]);
 
-app.controller('myCtrl', function($scope, $location, anchorSmoothScroll) {
+app.controller('myCtrl', function($scope, $location, anchorSmoothScroll,$http,$sce) {
+
   $scope.gotoElement = function (eID){
-    // set the location.hash to the id of
-    // the element you wish to scroll to.
     $location.hash('bottom');
-
-    // call $anchorScroll()
     anchorSmoothScroll.scrollTo(eID);
-
-    
-
   };
 
+angular.element(document).ready(function() {
+  var $form = $('#mc-embedded-subscribe-form')
+  if ($form.length > 0) {
+    $('form input[type="submit"]').bind('click', function(event) {
+      if (event) event.preventDefault()
+      register($form)
+    })
+  }
 });
+
+});
+
+
+
+function register($form) {
+  $('#mc-embedded-subscribe').val('Sending...');
+  $.ajax({
+    type: $form.attr('method'),
+    url: $form.attr('action'),
+    data: $form.serialize(),
+    cache: false,
+    dataType: 'json',
+    contentType: 'application/json; charset=utf-8',
+    error: function (err) { alert('Could not connect to the registration server. Please try again later.') },
+    success: function (data) {
+      $('#mc-embedded-subscribe').val('WHITE PAPER')
+      if (data.result === 'success') {
+        // Yeahhhh Success
+        console.log(data.msg)
+        $('#mce-EMAIL').css('borderColor', '#ffffff')
+        $('#subscribe-result').css('color', 'rgb(53, 114, 210)')
+        $('#subscribe-result').html('<p>Thank you for subscribing. We have sent you a confirmation email.</p>')
+        $('#mce-EMAIL').val('');
+        window.open('BitClaims_WhitePaper_2.FINAL.pdf', 'name');
+      } else {
+        // Something went wrong, do something to notify the user.
+        console.log(data.msg)
+        $('#mce-EMAIL').css('borderColor', '#ff8282')
+        $('#subscribe-result').css('color', '#ff8282')
+        $('#subscribe-result').html('<p>' + data.msg.substring(0) + '</p>')
+        window.open('BitClaims_WhitePaper_2.FINAL.pdf', 'name');
+      }
+    }
+  })
+};
